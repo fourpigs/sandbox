8faf6b9917a70094b88cb462e3fe93a1998ee33d
Yutaka Tabushi <fourpigs21@gmail.com>
2018-05-09 09:24:58 +0900

    add basic Eureka setting

--- hello-eureka/src/main/java/com/example/helloeureka/HelloEurekaApplication.java
+++ hello-eureka/src/main/java/com/example/helloeureka/HelloEurekaApplication.java
@@ -2,7 +2,9 @@ package com.example.helloeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class HelloEurekaApplication {
 
