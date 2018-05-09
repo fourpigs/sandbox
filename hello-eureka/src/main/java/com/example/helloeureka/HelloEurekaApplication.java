5bd90268fdaa4bf8e0e836cd5819480045fe97ae
Yutaka Tabushi <fourpigs21@gmail.com>
2018-05-09 09:21:11 +0900

    get EurekaServer from start.spring.io

---
+++ hello-eureka/src/main/java/com/example/helloeureka/HelloEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.example.helloeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEurekaApplication.class, args);
+	}
+}
