dae22d2f6109d2a07f43431b182daa3be2f00b85
Yutaka Tabushi <fourpigs21@gmail.com>
2018-05-14 12:44:23 +0900

    add Config Server

--- hello-spring-cloud/pen/src/main/java/com/example/pen/PenApplication.java
+++ hello-spring-cloud/pen/src/main/java/com/example/pen/PenApplication.java
@@ -1,7 +1,9 @@
 package com.example.pen;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +11,24 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @SpringBootApplication
 @RestController
-public class PenApplication {
 
-	@RequestMapping("/")
-	String pen() {
-		return "pen";
-	}
+public class PenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PenApplication.class, args);
 	}
+
+	@RefreshScope
+	@RestController
+	class MessageRestController {
+
+		@Value("${message:Hello default}")
+		private String message;
+
+		@RequestMapping("/")
+		String pen() {
+			return this.message;
+		}
+	}
+
 }
